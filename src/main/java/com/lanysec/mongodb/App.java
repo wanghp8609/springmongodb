78f490fa21e4addfbde336a3598c64506ed19bda
wanghp8609 <wanghp_8609@163.com>
2017-11-02 09:47:21 +0800

    Initial commit

---
+++ src/main/java/com/lanysec/mongodb/App.java
@@ -0,0 +1,13 @@
+package com.lanysec.mongodb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
