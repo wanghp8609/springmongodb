5509a054c4aa94e8dd5f5efb48a34c5910f656a6
wanghp8609 <wanghp_8609@163.com>
2017-11-02 11:23:43 +0800

    add new file

---
+++ src/test/java/com/lanysec/mongodb/SpringMongoTest.java
@@ -0,0 +1,26 @@
+package com.lanysec.mongodb;
+
+import com.lanysec.mongodb.po.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+public class SpringMongoTest {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Before
+    public void testBefore(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        mongoTemplate = (MongoTemplate)context.getBean("mongoTemplate");
+    }
+
+    @Test
+    public void testUserAdd(){
+        User user = new User();
+        user.setName("wanghp");
+        mongoTemplate.insert(user);
+    }
+}
